61f1fccadaa3bc9e594f1d758445b988aa5a45f5
govindarajan2003 <govindarajan14092003@gmail.com>
2023-04-01 12:27:26 +0530

    new

--- ezpark/app/migrations/0001_initial.py
+++ ezpark/app/migrations/0001_initial.py
@@ -12,16 +12,51 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Member',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+            Cname='Client',
+            Cfields=[
+                ('cid', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 
-                ('firstname', models.CharField(max_length=255)),
-                ('lastname', models.CharField(max_length=255)),
+                ('fullname', models.CharField(max_length=100)),
+                ('email',models.CharField(max_length=100)),            
+                ('phone',models.IntegerField(max_length=100)),
+                ('password',models.CharField(max_length=15)),
+                ('booking_status',models.CharField(max_length=100)),
+                ('fine',models.IntegerField(max_length=100)),
                 
-                ('phone',models.IntegerField(max_length=12)),
-                ('password',models.IntegerField(max_length=12)),
-                
+            ],
+            LOname='LandOwner',
+            LOfields=[
+                ('la_id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),               
+                ('fullname', models.CharField(max_length=100)),
+                ('email',models.CharField(max_length=100)),            
+                ('phone',models.IntegerField(max_length=100)),
+                ('password',models.CharField(max_length=15)),
+                ('booking_status',models.CharField(max_length=100)),
+                ('pincode',models.CharField(max_length=100)),
+                ('landmark',models.CharField(max_length=100))
+
+            ],
+            Lname='Land',
+            Lfields=[
+                ('l_id',models.AutoField(primary_key=True)),
+                ('pincode',models.IntegerField(max_length=100)),
+                ('sq_feet',models.FloatField(max_length=100)),
+                ('booking_status',models.CharField(max_length=100)),
+
+
+            ],
+            Vname='Vehicle',
+            Vfields=[
+                ('v_id',models.AutoField(primary_key=True)),
+                ('v_number',models.CharField(max_length=100)),
+                ('v_type',models.CharField(max_length=100))
+            
+            ],
+            Bname='Booking',
+            Bfields=[
+                ('B_id',models.AutoField(primary_key=True)),
+                ('booking_status',models.CharField(max_length=100))
+
             ],
         ),
     ]
--- ezpark/app/views.py
+++ ezpark/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Member
+from .models import *
 
 # Create your views here.
 
@@ -16,17 +16,18 @@ def members(request):
 
 def login(request):
    if request.method == "POST":
-      username = request.POST['uname']
+      user_name = request.POST['uname']
       password = request.POST['psw']
-      usernamecheck = Member.objects.filter(firstname=username)
-      passwordcheck = Member.objects.filter(password=password)
+      usernamecheck = Client.objects.filter(email = user_name)
+      passwordcheck = Client.objects.filter(password = password)
       if(usernamecheck.count() != 0 and passwordcheck.count()!=0):
          return render(request, "loginhome.html")
       else:
         return HttpResponse("not logged in")
-      print(username)
+      print(user_name)
    return render(request, "login.html")
 
 def signup(request):
+   
    temp= loader.get_template('reg.html')
    return HttpResponse(temp.render())
